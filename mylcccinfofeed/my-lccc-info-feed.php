dbf168ab89d40c5d4d7983ffc53ffcc6a7899b69
David Michael Brattoli <dbrattoli@lorainccc.edu>
2016-02-03 08:46:05 -0500

    Updated the code to include metaboxes

--- mylcccinfofeed/my-lccc-info-feed.php
+++ mylcccinfofeed/my-lccc-info-feed.php
@@ -108,7 +108,7 @@ function lcccanouncement_post_type() {
 		'label'                 => __( 'LCCC Anouncement', 'text_domain' ),
 		'description'           => __( 'Post Type Description', 'text_domain' ),
 		'labels'                => $labels,
-		'supports'              => array( 'title', 'editor', 'excerpt', 'author', 'thumbnail', 'trackbacks', 'revisions', 'custom-fields', 'page-attributes', 'post-formats', ),
+		'supports'              => array( 'title', 'editor', 'excerpt', 'thumbnail', 'revisions', 'page-attributes', ),
 		'taxonomies'            => array( 'category', 'post_tag' ),
 		'hierarchical'          => false,
 		'public'                => true,
@@ -167,7 +167,7 @@ function lcccevents_post_type() {
 		'label'                 => __( 'LCCC Event', 'text_domain' ),
 		'description'           => __( 'Post Type Description', 'text_domain' ),
 		'labels'                => $labels,
-		'supports'              => array( 'title', 'editor', 'excerpt', 'author', 'thumbnail', 'trackbacks', 'revisions', 'custom-fields', 'page-attributes', 'post-formats', ),
+		'supports'              => array( 'title', 'editor', 'excerpt', 'thumbnail', 'revisions', 'page-attributes',),
 		'taxonomies'            => array( 'category', 'post_tag' ),
 		'hierarchical'          => false,
 		'public'                => true,
@@ -191,3 +191,99 @@ function lcccevents_post_type() {
 }
 add_action( 'init', 'lcccevents_post_type', 0 );
 
+/* Fire our meta box setup function on the post editor screen. */
+add_action( 'load-post.php', 'mylccc_post_meta_boxes_setup' );
+add_action( 'load-post-new.php', 'mylccc_post_meta_boxes_setup' );
+
+/* Meta box setup function. */
+function mylccc_post_meta_boxes_setup() {
+
+  /* Add meta boxes on the 'add_meta_boxes' hook. */
+  add_action( 'add_meta_boxes', 'mylccc_add_post_meta_boxes' );
+	
+	/* Save post meta on the 'save_post' hook. */
+  add_action( 'save_post', 'mylccc_save_post_class_meta', 10, 2 );
+}
+
+/* Create one or more meta boxes to be displayed on the post editor screen. */
+function mylccc_add_post_meta_boxes() {
+
+  add_meta_box(
+    'mylccc-event-location',      // Unique ID
+    esc_html__( 'Location', 'example' ),    // Title
+    'mylccc_post_class_meta_box',   // Callback function
+    array('lccc_announcement','lccc_event' ),         // Admin page (or post type)
+    'advanced',         // Context
+    'core'         // Priority
+  );
+
+}
+
+/* Display the post meta box. */
+function mylccc_post_class_meta_box( $object, $box ) { ?>
+
+  <?php wp_nonce_field( basename( __FILE__ ), 'mylccc_event_location_nonce' ); ?>
+  <p>
+    <label for="mylccc-event-location"><?php _e( "Add the event's location.", 'example' ); ?></label>
+    <br />
+    <input class="widefat" type="text" name="mylccc-event-location" id="mylccc-event-location" value="<?php echo esc_attr( get_post_meta( $object->ID, 'mylccc-event-location', true ) ); ?>" size="30" />
+  </p>
+<?php }
+/* Save the meta box's post metadata. */
+function mylccc_save_post_class_meta( $post_id, $post ) {
+
+  /* Verify the nonce before proceeding. */
+  if ( !isset( $_POST['mylccc_event_location_nonce'] ) || !wp_verify_nonce( $_POST['mylccc_event_location_nonce'], basename( __FILE__ ) ) )
+    return $post_id;
+
+  /* Get the post type object. */
+  $post_type = get_post_type_object( $post->post_type );
+
+  /* Check if the current user has permission to edit the post. */
+  if ( !current_user_can( $post_type->cap->edit_post, $post_id ) )
+    return $post_id;
+
+  /* Get the posted data and sanitize it for use as an HTML class. */
+  $new_meta_value = ( isset( $_POST['mylccc-event-location'] ) ? sanitize_html_class( $_POST['mylccc-event-location'] ) : '' );
+
+  /* Get the meta key. */
+  $meta_key = 'mylccc_post_class';
+
+  /* Get the meta value of the custom field key. */
+  $meta_value = get_post_meta( $post_id, $meta_key, true );
+
+  /* If a new meta value was added and there was no previous value, add it. */
+  if ( $new_meta_value && '' == $meta_value )
+    add_post_meta( $post_id, $meta_key, $new_meta_value, true );
+
+  /* If the new meta value does not match the old value, update it. */
+  elseif ( $new_meta_value && $new_meta_value != $meta_value )
+    update_post_meta( $post_id, $meta_key, $new_meta_value );
+
+  /* If there is no new meta value but an old value exists, delete it. */
+  elseif ( '' == $new_meta_value && $meta_value )
+    delete_post_meta( $post_id, $meta_key, $meta_value );
+}
+/* Filter the post class hook with our custom post class function. */
+add_filter( 'post_class', 'mylccc_post_class' );
+
+function mylccc_post_class( $classes ) {
+
+  /* Get the current post ID. */
+  $post_id = get_the_ID();
+
+  /* If we have a post ID, proceed. */
+  if ( !empty( $post_id ) ) {
+
+    /* Get the custom post class. */
+    $post_class = get_post_meta( $post_id, 'mylccc_post_class', true );
+
+    /* If a post class was input, sanitize it and add it to the post class array. */
+    if ( !empty( $post_class ) )
+      $classes[] = sanitize_html_class( $post_class );
+  }
+
+  return $classes;
+}
+
+?>
