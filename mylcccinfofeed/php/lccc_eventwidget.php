851b599d2f148e05161bfd4ac121ce5149b49202
David Michael Brattoli <dbrattoli@lorainccc.edu>
2016-02-24 10:55:33 -0500

    Final Commit of finished code
    This commit includes the complete plug-in which includes
    3 Post Types: Events, Announcements, Locations
    Meta-box code for custom post types
    2 Widgets: one for displaying events and the other to display
    announcements. Each has the ability to modify number of posts shown and
    what category to pull the posts from.

---
+++ mylcccinfofeed/php/lccc_eventwidget.php
@@ -0,0 +1,183 @@
+<?php
+/** Widget Code */
+class LCCC_Whats_Going_On_Event_Widget extends WP_Widget {
+
+	/**
+	 * Sets up the widgets name etc
+	 */
+	public function __construct() {
+		$widget_ops = array( 
+				'classname' => 'LCCC_Whats_Going_On_Event_Widget',
+			'description' => 'LCCC Whats Going On Event Widget fpr displaying LCCC Events on LCCC sites',
+		);
+		parent::__construct( 'LCCC_Whats_Going_On_Event_Widgett', 'LCCC Whats Going On Event Widget', $widget_ops );
+	}
+
+	/**
+	 * Outputs the content of the widget
+	 *
+	 * @param array $args
+	 * @param array $instance
+	 */
+	public function widget( $args, $instance ) {
+		// outputs the content of the widget
+  extract( $args );
+   // these are the widget options
+			$numberofposts = $instance['numberofposts']; 
+			$whattodisplay = 'lccc_event';
+			$widgetcategory = $instance['category'];
+   echo $before_widget;
+   // Display the widget
+		 echo '<div class="small-12 medium-12 large-12 columns '.$whattodisplay.'">';
+		 if ($whattodisplay == 'lccc_event'){
+   echo '<div class="small-12 medium-12 large-12 columns '.$whattodisplay.'_header">';
+							echo '<div class="small-12 medium-4 large-4 columns '.$whattodisplay.' headerlogo">';
+											echo '<i class="lccc-font-lccc-reverse">'.'</i>';
+							echo '</div>';
+							echo '<div class="small-12 medium-8 large-8 columns ">';
+										echo '<h2 class="headertext">'.'Events'.'</h2>';
+							echo '</div>';
+			echo '</div>';
+			}
+	  
+ if ($whattodisplay == 'lccc_event'){
+					$eventargs=array(
+					'post_type' => $whattodisplay,
+					'post_status' => 'publish',
+  			'posts_per_page' => $numberofposts,
+					'order' => 'DESC',
+					'category_name' => $widgetcategory
+					);
+					$newevents = new WP_Query($eventargs);
+					if ( $newevents->have_posts() ) :
+									while ( $newevents->have_posts() ) : $newevents->the_post();
+			echo '<div class="small-12 medium-12 large-12 columns eventcontainer">';
+								echo '<div class="small-12 medium-3 large-3 columns calender">';
+												$eventdate = event_meta_box_get_meta(
+'event_meta_box_event_start_date_and_time_');
+												$date=strtotime($eventdate);
+												$month=date("M",$date);
+												$day=date("d",$date);
+								echo '<p class="month">'.$month.'</p>';
+								echo '<p class="day">'.$day.'</p>';
+								echo '</div>';
+								echo '<div class="small-12 medium-9 large-9 columns">';
+  						?>
+								<a href="<?php the_permalink();?>"><?php the_title('<h3 class="eventtitle">','</h3>');?></a>
+								<?php
+											the_excerpt('<p>','</p>');
+								echo '</div>';
+								echo '</div>';
+							endwhile;
+					endif;
+		}
+		if ($whattodisplay == 'lccc_announcement'){
+					$announcementargs=array(
+					'post_type' => $whattodisplay,
+					'post_status' => 'publish',
+  			'posts_per_page' => $numberofposts,
+					'order' => 'DESC',
+					'category_name' => $widgetcategory
+					);
+					$newevents = new WP_Query($announcementargs);
+					if ( $newevents->have_posts() ) :
+									while ( $newevents->have_posts() ) : $newevents->the_post();
+			echo '<div class="small-12 medium-12 large-12 columns eventcontainer">';
+								echo '<div class="small-12 medium-3 large-3 columns eventhumbnail">';
+												the_post_thumbnail();
+								echo '</div>';
+								echo '<div class="small-12 medium-9 large-9 columns">';
+  						?>
+								<a href="<?php the_permalink();?>"><?php the_title('<h3 class="eventtitle">','</h3>');?></a>
+								<?php
+											the_excerpt('<p>','</p>');
+								echo '</div>';
+								echo '</div>';
+							endwhile;
+					endif;
+		}
+		echo '</div>';
+		
+		if ($whattodisplay == 'lccc_event'){
+					$currentpostype = 'Events';
+			echo '<div class="small-12 medium-12 large-12 columns">';
+							echo '<a href="'.get_post_type_archive_link( $whattodisplay ).'" class="button expand">View All '.$currentpostype .'</a>';
+		echo '</div>';
+		}
+		if ($whattodisplay == 'lccc_location'){
+					$currentpostype = 'Locations';
+			echo '<div class="small-12 medium-12 large-12 columns">';
+							echo '<a href="'.get_post_type_archive_link( $whattodisplay ).'" class="button expand">View All '.$currentpostype .'</a>';
+		echo '</div>';
+		}
+		
+  echo $after_widget;
+	}
+
+	/**
+	 * Outputs the options form on admin
+	 *
+	 * @param array $instance The widget options
+	 */
+	public function form( $instance ) {
+		// outputs the options form on admin
+		
+// Check values
+if( $instance) {
+					$numberofposts = esc_attr($instance['numberofposts']);
+					$widgetcategory = esc_attr($instance['category']);
+} else {
+					$numberofposts = '';
+					$widgetcategory = '';
+}
+?>
+
+<p>
+<label for="<?php echo $this->get_field_id('numberofposts'); ?>"><?php _e('Number of posts', 'wp_widget_plugin'); ?></label>
+<select name="<?php echo $this->get_field_name('numberofposts'); ?>" id="<?php echo $this->get_field_id('numberofposts'); ?>">
+<?php
+$options = array('select..',5, 10, 15);
+foreach ($options as $option) {
+echo '<option value="' . $option . '" id="' . $option . '"', $numberofposts == $option ? ' selected="selected"' : '', '>', $option, '</option>';
+}
+?>
+</select>
+</p>
+
+		<p>
+<label for="<?php echo $this->get_field_id('category'); ?>"><?php _e('Category:', 'wp_widget_plugin'); ?></label>
+
+<select name="<?php echo $this->get_field_name('category'); ?>" id="<?php echo $this->get_field_id('category'); ?>"class="widefat">
+		<?php
+  // Get categories as array
+  $categories = get_categories( $args );
+  foreach ( $categories as $category ) :
+  		echo '<option value="' . $category->name . '" id="' . $category->term_id . '"', $widgetcategory == $category->name ? ' selected="selected"' : '', '>', $category->name, '</option>';
+  endforeach;
+?>
+</select>		
+		
+</p>
+<?php
+	}
+
+	/**
+	 * Processing widget options on save
+	 *
+	 * @param array $new_instance The new options
+	 * @param array $old_instance The previous options
+	 */
+	public function update( $new_instance, $old_instance ) {
+		// processes widget options to be saved
+		       $instance = $old_instance;
+      // Fields
+     	$instance['numberofposts'] = strip_tags($new_instance['numberofposts']);
+							$instance['whattodisplay'] = strip_tags($new_instance['whattodisplay']);
+		 $instance['category'] = strip_tags($new_instance['category']);
+						return $instance;
+	}
+}
+add_action( 'widgets_init', function(){
+	register_widget( 'LCCC_Whats_Going_On_Event_Widget' );
+});
+?>
---
+++ mylcccinfofeed/php/lccc_posttypes.php
@@ -0,0 +1,175 @@
+<?php
+// Register Custom Post Type
+function lcccanouncement_post_type() {
+
+	$labels = array(
+		'name'                  => _x( 'LCCC Anouncements', 'Post Type General Name', 'text_domain' ),
+		'singular_name'         => _x( 'LCCC Anouncement', 'Post Type Singular Name', 'text_domain' ),
+		'menu_name'             => __( 'LCCC Anouncements', 'text_domain' ),
+		'name_admin_bar'        => __( 'LCCC Anouncements', 'text_domain' ),
+		'archives'              => __( 'LCCC Anouncements Archives', 'text_domain' ),
+		'parent_item_colon'     => __( 'Parent Item:', 'text_domain' ),
+		'all_items'             => __( 'All LCCC Anouncements', 'text_domain' ),
+		'add_new_item'          => __( 'Add New LCCC Anouncement', 'text_domain' ),
+		'add_new'               => __( 'Add New LCCC Anouncement', 'text_domain' ),
+		'new_item'              => __( 'New LCCC Anouncement', 'text_domain' ),
+		'edit_item'             => __( 'Edit LCCC Anouncement', 'text_domain' ),
+		'update_item'           => __( 'Update LCCC Anouncement', 'text_domain' ),
+		'view_item'             => __( 'View LCCC Anouncements', 'text_domain' ),
+		'search_items'          => __( 'Search LCCC Anouncements', 'text_domain' ),
+		'not_found'             => __( 'Not found', 'text_domain' ),
+		'not_found_in_trash'    => __( 'Not found in Trash', 'text_domain' ),
+		'featured_image'        => __( 'Featured Image', 'text_domain' ),
+		'set_featured_image'    => __( 'Set featured image', 'text_domain' ),
+		'remove_featured_image' => __( 'Remove featured image', 'text_domain' ),
+		'use_featured_image'    => __( 'Use as featured image', 'text_domain' ),
+		'insert_into_item'      => __( 'Insert into Anouncement', 'text_domain' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this Anouncement', 'text_domain' ),
+		'items_list'            => __( 'LCCC Anouncements list', 'text_domain' ),
+		'items_list_navigation' => __( 'LCCC Anouncements navigation', 'text_domain' ),
+		'filter_items_list'     => __( 'Filter items list', 'text_domain' ),
+	);
+	$args = array(
+		'label'                 => __( 'LCCC Anouncement', 'text_domain' ),
+		'description'           => __( 'Post Type Description', 'text_domain' ),
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'editor', 'thumbnail', 'revisions', 'page-attributes', 'custom-fields' ),
+		'taxonomies'            => array( 'category', 'post_tag' ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'show_in_rest'       => true,
+  'rest_base'          => 'books-api',
+  'rest_controller_class' => 'WP_REST_Posts_Controller',
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,
+		'menu_icon'   => 'dashicons-megaphone',
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'post',
+	);
+	register_post_type( 'lccc_announcement', $args );
+
+}
+add_action( 'init', 'lcccanouncement_post_type', 0 );
+
+// Register Custom Post Type
+function lcccevents_post_type() {
+
+	$labels = array(
+		'name'                  => _x( 'LCCC  Events', 'Post Type General Name', 'text_domain' ),
+		'singular_name'         => _x( 'LCCC Event', 'Post Type Singular Name', 'text_domain' ),
+		'menu_name'             => __( 'LCCC  Events', 'text_domain' ),
+		'name_admin_bar'        => __( 'LCCC  Events', 'text_domain' ),
+		'archives'              => __( 'LCCC  Events Archives', 'text_domain' ),
+		'parent_item_colon'     => __( 'Parent Item:', 'text_domain' ),
+		'all_items'             => __( 'All LCCC  Events', 'text_domain' ),
+		'add_new_item'          => __( 'Add New LCCC  Event', 'text_domain' ),
+		'add_new'               => __( 'Add New LCCC  Event', 'text_domain' ),
+		'new_item'              => __( 'New LCCC  Event', 'text_domain' ),
+		'edit_item'             => __( 'Edit LCCC  Event', 'text_domain' ),
+		'update_item'           => __( 'Update LCCC  Event', 'text_domain' ),
+		'view_item'             => __( 'View LCCC  Events', 'text_domain' ),
+		'search_items'          => __( 'Search LCCC  Events', 'text_domain' ),
+		'not_found'             => __( 'Not found', 'text_domain' ),
+		'not_found_in_trash'    => __( 'Not found in Trash', 'text_domain' ),
+		'featured_image'        => __( 'Featured Image', 'text_domain' ),
+		'set_featured_image'    => __( 'Set featured image', 'text_domain' ),
+		'remove_featured_image' => __( 'Remove featured image', 'text_domain' ),
+		'use_featured_image'    => __( 'Use as featured image', 'text_domain' ),
+		'insert_into_item'      => __( 'Insert into  Event', 'text_domain' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this  Event', 'text_domain' ),
+		'items_list'            => __( 'LCCC  Events list', 'text_domain' ),
+		'items_list_navigation' => __( 'LCCC  Events navigation', 'text_domain' ),
+		'filter_items_list'     => __( 'Filter items list', 'text_domain' ),
+	);
+	$args = array(
+		'label'                 => __( 'LCCC Event', 'text_domain' ),
+		'description'           => __( 'Post Type Description', 'text_domain' ),
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'editor', 'thumbnail', 'revisions', 'page-attributes',),
+		'taxonomies'            => array( 'category', 'post_tag' ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 4,
+		'show_in_rest'       => true,
+  'rest_base'          => 'books-api',
+  'rest_controller_class' => 'WP_REST_Posts_Controller',
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,
+		'menu_icon'   => 'dashicons-calendar-alt',
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'post',
+	);
+	register_post_type( 'lccc_event', $args );
+
+}
+add_action( 'init', 'lcccevents_post_type', 0 );
+// Register Custom Post Type
+function lcccLocation_post_type() {
+
+	$labels = array(
+		'name'                  => _x( 'LCCC Locations', 'Post Type General Name', 'text_domain' ),
+		'singular_name'         => _x( 'LCCC Location', 'Post Type Singular Name', 'text_domain' ),
+		'menu_name'             => __( 'LCCC Locations', 'text_domain' ),
+		'name_admin_bar'        => __( 'LCCC Locations', 'text_domain' ),
+		'archives'              => __( 'LCCC Locations Archives', 'text_domain' ),
+		'parent_item_colon'     => __( 'Parent Item:', 'text_domain' ),
+		'all_items'             => __( 'All LCCC Locations', 'text_domain' ),
+		'add_new_item'          => __( 'Add New LCCC Location', 'text_domain' ),
+		'add_new'               => __( 'Add New LCCC Location', 'text_domain' ),
+		'new_item'              => __( 'New LCCC Location', 'text_domain' ),
+		'edit_item'             => __( 'Edit LCCC Location', 'text_domain' ),
+		'update_item'           => __( 'Update LCCC Location', 'text_domain' ),
+		'view_item'             => __( 'View LCCC Locations', 'text_domain' ),
+		'search_items'          => __( 'Search LCCC Locations', 'text_domain' ),
+		'not_found'             => __( 'Not found', 'text_domain' ),
+		'not_found_in_trash'    => __( 'Not found in Trash', 'text_domain' ),
+		'featured_image'        => __( 'Featured Image', 'text_domain' ),
+		'set_featured_image'    => __( 'Set featured image', 'text_domain' ),
+		'remove_featured_image' => __( 'Remove featured image', 'text_domain' ),
+		'use_featured_image'    => __( 'Use as featured image', 'text_domain' ),
+		'insert_into_item'      => __( 'Insert into Location', 'text_domain' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this Location', 'text_domain' ),
+		'items_list'            => __( 'LCCC Locations list', 'text_domain' ),
+		'items_list_navigation' => __( 'LCCC Locations navigation', 'text_domain' ),
+		'filter_items_list'     => __( 'Filter items list', 'text_domain' ),
+	);
+	$args = array(
+		'label'                 => __( 'LCCC Location', 'text_domain' ),
+		'description'           => __( 'Post Type Description', 'text_domain' ),
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'editor', 'thumbnail', 'revisions', 'page-attributes', ),
+		'taxonomies'            => array( 'category', 'post_tag' ),
+		'hierarchical'          => true,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'show_in_rest'       => true,
+  'rest_base'          => 'books-api',
+  'rest_controller_class' => 'WP_REST_Posts_Controller',
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,
+		'menu_icon'   => 'dashicons-location-alt',
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'page',
+	);
+	register_post_type( 'lccc_location', $args );
+
+}
+add_action( 'init', 'lcccLocation_post_type', 0 );
+
+?>
