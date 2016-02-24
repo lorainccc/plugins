e48c92b0875d85cb5af0494125c42ded64356939
David Michael Brattoli <dbrattoli@lorainccc.edu>
2016-02-24 09:57:40 -0500

    02/24/16 Updated Code
    Updated Code with everything. Still needs functions to be split into
    individual files.

--- mylcccinfofeed/my-lccc-info-feed.php
+++ mylcccinfofeed/my-lccc-info-feed.php
@@ -538,17 +538,17 @@ add_action( 'save_post', 'event_meta_box_save' );
 
 
 /** Widget Code */
-class LCCC_Whats_Going_On_Widget extends WP_Widget {
+class LCCC_Whats_Going_On_Announcement_Widget extends WP_Widget {
 
 	/**
 	 * Sets up the widgets name etc
 	 */
 	public function __construct() {
 		$widget_ops = array( 
-			'classname' => 'LCCC_Whats_Going_On_Widget',
-			'description' => 'LCCC Whats Going On Widget is awesome',
+			'classname' => 'LCCC_Whats_Going_On_Announcement_Widget',
+			'description' => 'LCCC Whats Going On Announcement Widget fpr displaying LCCC Announcements on LCCC sites',
 		);
-		parent::__construct( 'LCCC_Whats_Going_On_Widget', 'LCCC Whats Going On Widget', $widget_ops );
+		parent::__construct( 'LCCC_Whats_Going_On_Announcement_Widget', 'LCCC Whats Going On Announcement Widget', $widget_ops );
 	}
 
 	/**
@@ -561,9 +561,8 @@ class LCCC_Whats_Going_On_Widget extends WP_Widget {
 		// outputs the content of the widget
   extract( $args );
    // these are the widget options
-			$select = $instance['select'];
 			$numberofposts = $instance['numberofposts']; 
-			$whattodisplay = $instance['whattodisplay'];
+			$whattodisplay = 'lccc_announcement';
 			$widgetcategory = $instance['category'];
    echo $before_widget;
    // Display the widget
@@ -581,6 +580,147 @@ class LCCC_Whats_Going_On_Widget extends WP_Widget {
 		if ($whattodisplay == 'lccc_announcement'){
    echo '<div class="small-12 medium-12 large-12 columns '.$whattodisplay.'_header">';
 						echo '<h2 class="announcementheader">'.'In The News'.'</h2>';				
+			echo '</div>';
+			}
+	  
+		if ($whattodisplay == 'lccc_announcement'){
+					$announcementargs=array(
+					'post_type' => $whattodisplay,
+					'post_status' => 'publish',
+  			'posts_per_page' => $numberofposts,
+					'order' => 'DESC',
+					'category_name' => $widgetcategory
+					);
+					$newevents = new WP_Query($announcementargs);
+					if ( $newevents->have_posts() ) :
+									while ( $newevents->have_posts() ) : $newevents->the_post();
+			echo '<div class="small-12 medium-12 large-12 columns eventcontainer">';
+								echo '<div class="small-12 medium-3 large-3 columns eventhumbnail">';
+												the_post_thumbnail();
+								echo '</div>';
+								echo '<div class="small-12 medium-9 large-9 columns">';
+  						?>
+								<a href="<?php the_permalink();?>"><?php the_title('<h3 class="eventtitle">','</h3>');?></a>
+								<?php
+											the_excerpt('<p>','</p>');
+								echo '</div>';
+								echo '</div>';
+							endwhile;
+					endif;
+		}
+		if ($whattodisplay == 'lccc_announcement'){
+					$currentpostype = 'Announcments';
+			echo '<div class="small-12 medium-12 large-12 columns">';
+							echo '<a href="'.get_post_type_archive_link( $whattodisplay ).'" class="button">View All '.$currentpostype .'</a>';
+		echo '</div>';
+		}
+		echo '</div>';
+  echo $after_widget;
+	}
+
+	/**
+	 * Outputs the options form on admin
+	 *
+	 * @param array $instance The widget options
+	 */
+	public function form( $instance ) {
+		// outputs the options form on admin
+		
+// Check values
+if( $instance) {
+					$numberofposts = esc_attr($instance['numberofposts']);
+					$widgetcategory = esc_attr($instance['category']);
+} else {
+					$numberofposts = '';
+					$widgetcategory = '';
+}
+?>
+
+<p>
+<label for="<?php echo $this->get_field_id('numberofposts'); ?>"><?php _e('Number of posts', 'wp_widget_plugin'); ?></label>
+<select name="<?php echo $this->get_field_name('numberofposts'); ?>" id="<?php echo $this->get_field_id('numberofposts'); ?>">
+<?php
+$options = array('select..',5, 10, 15);
+foreach ($options as $option) {
+echo '<option value="' . $option . '" id="' . $option . '"', $numberofposts == $option ? ' selected="selected"' : '', '>', $option, '</option>';
+}
+?>
+</select>
+</p>
+		<p>
+<label for="<?php echo $this->get_field_id('category'); ?>"><?php _e('Category:', 'wp_widget_plugin'); ?></label>
+
+<select name="<?php echo $this->get_field_name('category'); ?>" id="<?php echo $this->get_field_id('category'); ?>"class="widefat">
+		<?php
+  // Get categories as array
+  $categories = get_categories( $args );
+  foreach ( $categories as $category ) :
+  		echo '<option value="' . $category->name . '" id="' . $category->term_id . '"', $widgetcategory == $category->name ? ' selected="selected"' : '', '>', $category->name, '</option>';
+  endforeach;
+?>
+</select>		
+		
+</p>
+<?php
+	}
+
+	/**
+	 * Processing widget options on save
+	 *
+	 * @param array $new_instance The new options
+	 * @param array $old_instance The previous options
+	 */
+	public function update( $new_instance, $old_instance ) {
+		// processes widget options to be saved
+		       $instance = $old_instance;
+      // Fields
+     	$instance['numberofposts'] = strip_tags($new_instance['numberofposts']);
+		 $instance['category'] = strip_tags($new_instance['category']);
+						return $instance;
+	}
+}
+add_action( 'widgets_init', function(){
+	register_widget( 'LCCC_Whats_Going_On_Announcement_Widget' );
+});
+
+/** Widget Code */
+class LCCC_Whats_Going_On_Event_Widget extends WP_Widget {
+
+	/**
+	 * Sets up the widgets name etc
+	 */
+	public function __construct() {
+		$widget_ops = array( 
+				'classname' => 'LCCC_Whats_Going_On_Event_Widget',
+			'description' => 'LCCC Whats Going On Event Widget fpr displaying LCCC Events on LCCC sites',
+		);
+		parent::__construct( 'LCCC_Whats_Going_On_Event_Widgett', 'LCCC Whats Going On Event Widget', $widget_ops );
+	}
+
+	/**
+	 * Outputs the content of the widget
+	 *
+	 * @param array $args
+	 * @param array $instance
+	 */
+	public function widget( $args, $instance ) {
+		// outputs the content of the widget
+  extract( $args );
+   // these are the widget options
+			$numberofposts = $instance['numberofposts']; 
+			$whattodisplay = 'lccc_event';
+			$widgetcategory = $instance['category'];
+   echo $before_widget;
+   // Display the widget
+		 echo '<div class="small-12 medium-12 large-12 columns '.$whattodisplay.'">';
+		 if ($whattodisplay == 'lccc_event'){
+   echo '<div class="small-12 medium-12 large-12 columns '.$whattodisplay.'_header">';
+							echo '<div class="small-12 medium-4 large-4 columns '.$whattodisplay.' headerlogo">';
+											echo '<i class="lccc-font-lccc-reverse">'.'</i>';
+							echo '</div>';
+							echo '<div class="small-12 medium-8 large-8 columns ">';
+										echo '<h2 class="headertext">'.'Events'.'</h2>';
+							echo '</div>';
 			echo '</div>';
 			}
 	  
@@ -641,12 +781,7 @@ class LCCC_Whats_Going_On_Widget extends WP_Widget {
 					endif;
 		}
 		echo '</div>';
-		if ($whattodisplay == 'lccc_announcement'){
-					$currentpostype = 'Announcments';
-			echo '<div class="small-12 medium-12 large-12 columns">';
-							echo '<a href="'.get_post_type_archive_link( $whattodisplay ).'" class="button">View All '.$currentpostype .'</a>';
-		echo '</div>';
-		}
+		
 		if ($whattodisplay == 'lccc_event'){
 					$currentpostype = 'Events';
 			echo '<div class="small-12 medium-12 large-12 columns">';
@@ -674,11 +809,9 @@ class LCCC_Whats_Going_On_Widget extends WP_Widget {
 // Check values
 if( $instance) {
 					$numberofposts = esc_attr($instance['numberofposts']);
-					$whattodisplay = esc_attr($instance['whattodisplay']);
 					$widgetcategory = esc_attr($instance['category']);
 } else {
 					$numberofposts = '';
-					$whattodisplay = '';
 					$widgetcategory = '';
 }
 ?>
@@ -694,27 +827,7 @@ echo '<option value="' . $option . '" id="' . $option . '"', $numberofposts == $
 ?>
 </select>
 </p>
-		<p>
-<label for="<?php echo $this->get_field_id('whattodisplay'); ?>"><?php _e('What to display?', 'wp_widget_plugin'); ?></label>
-<select name="<?php echo $this->get_field_name('whattodisplay'); ?>" id="<?php echo $this->get_field_id('whattodisplay'); ?>"class="widefat">
-<?php
-$args = array(
-   'public'   => true,
-   '_builtin' => false
-);
-$output = 'names'; // names or objects, note names is the default
-$operator = 'and'; // 'and' or 'or'
-$post_types = get_post_types( $args, $output, $operator ); 
-$options = array();
-foreach ( $post_types  as $post_type ) {
-   $options[] = $post_type ;
-}
-foreach ($options as $option) {
-echo '<option value="' . $option . '" id="' . $option . '"', $whattodisplay == $option ? ' selected="selected"' : '', '>', $option, '</option>';
-}
-?>
-</select>
-</p>
+
 		<p>
 <label for="<?php echo $this->get_field_id('category'); ?>"><?php _e('Category:', 'wp_widget_plugin'); ?></label>
 
@@ -749,8 +862,9 @@ echo '<option value="' . $option . '" id="' . $option . '"', $whattodisplay == $
 	}
 }
 add_action( 'widgets_init', function(){
-	register_widget( 'LCCC_Whats_Going_On_Widget' );
+	register_widget( 'LCCC_Whats_Going_On_Event_Widget' );
 });
+
 
 // CHANGE EXCERPT LENGTH FOR DIFFERENT POST TYPES
  
