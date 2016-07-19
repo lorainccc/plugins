b5e3ce0f1f0e1999373642e5ccdf50112870694d
Joe Querin <joecue@gmail.com>
2016-07-19 18:29:11 -0400

    Adding ContactInfo Metabox and Breadcrumb Trail and Options

--- lccc-subsite-specific-plugin/php/dept-contact-metabox.php
+++ lccc-subsite-specific-plugin/php/dept-contact-metabox.php
@@ -1,7 +1,7 @@
 <?php
 /*
 		*	Code adapted from https://www.smashingmagazine.com/2011/10/create-custom-post-meta-boxes-wordpress
-		*	Created May 2016.
+		*	Created July 2016.
 		*
 		*/
 
@@ -42,37 +42,123 @@ function lc_show_dept_contact_info_meta_box( $object, $box ) { ?>
    <input type="text" name="lc_dept_extension_field" id="lc_dept_extension_field" value="<?php echo esc_attr( get_post_meta ( $object->ID, 'lc_dept_extension_field', true ) ); ?>" size="30" />
   </p>
 
+  <p>
+   <label for="lc_dept_email_field">
+    <?php _e( "Department Email Address: ", "lorainccc" ); ?>
+   </label>
+   <input type="text" name="lc_dept_email_field" id="lc_dept_email_field" value="<?php echo esc_attr( get_post_meta ( $object->ID, 'lc_dept_email_field', true ) ); ?>" size="30" />
+  </p>
+
+  <p>
+   <label for="lc_dept_fax_number_field">
+    <?php _e( "Department Fax Number: ", "lorainccc" ); ?>
+   </label>
+   <input type="text" name="lc_dept_fax_number_field" id="lc_dept_fax_number_field" value="<?php echo esc_attr( get_post_meta ( $object->ID, 'lc_dept_fax_number_field', true ) ); ?>" size="30" />
+  </p>
+
+  <p>
+   <label for="lc_dept_room_number_field">
+    <?php _e( "Department Building and Room Number: ", "lorainccc" ); ?>
+   </label>
+   <input type="text" name="lc_dept_room_number_field" id="lc_dept_room_number_field" value="<?php echo esc_attr( get_post_meta ( $object->ID, 'lc_dept_room_number_field', true ) ); ?>" size="30" />
+  </p>
+
+  <p>
+   <label for="lc_dept_office_hours_field">
+    <?php _e( "Department Office Hours: ", "lorainccc" ); ?>
+   </label>
+   <input type="text" name="lc_dept_office_hours_field" id="lc_dept_office_hours_field" value="<?php echo esc_attr( get_post_meta ( $object->ID, 'lc_dept_office_hours_field', true ) ); ?>" size="30" />
+  </p>
+
   <?php
 }
 
 /* Save the meta box's post metadata */
 function lc_dept_contact_save_info( $post_id, $post ) {
- 
+
  /* Verify the nonce before proceeding */
  if ( !isset( $_POST['lc_dept_contact_post_nonce'] ) || !wp_verify_nonce( $_POST['lc_dept_contact_post_nonce'], basename( __FILE__ ) ) )
   return $post_id;
- 
+
  /* Get the post type object */
  $post_type = get_post_type_object ( $post->post_type );
- 
+
  /* Check if the current user has permission to edit the post. */
  if ( !current_user_can( $post_type->cap->edit_post, $post_id ) )
   return $post_id;
- 
+
+ /* Extension Field */
  /* Get the posted data and sanitize it for use as a date value. */
  $new_meta_value = ( isset( $_POST['lc_dept_extension_field'] ) ? sanitize_text_field($_POST['lc_dept_extension_field'] ) : '' );
- 
+
  /* Get the meta key. */
+ $meta_key = 'lc_dept_extension_field';
+
+  /* Get the meta value of the custom field key. */
  $meta_value = get_post_meta ($post_id, $meta_key, true );
- 
- /* If a new meta value was added and there was no previous value, add it. */
+
+ update_post_meta( $post_id, $meta_key, $new_meta_value, $meta_value );
+
+ /* Email Field */
+ /* Get the posted data and sanitize it for use as a date value. */
+ $new_meta_value = ( isset( $_POST['lc_dept_email_field'] ) ? sanitize_text_field($_POST['lc_dept_email_field'] ) : '' );
+
+ /* Get the meta key. */
+ $meta_key = 'lc_dept_email_field';
+
+  /* Get the meta value of the custom field key. */
+ $meta_value = get_post_meta ($post_id, $meta_key, true );
+
+ update_post_meta( $post_id, $meta_key, $new_meta_value, $meta_value );
+
+ /* Fax Number Field */
+ /* Get the posted data and sanitize it for use as a date value. */
+ $new_meta_value = ( isset( $_POST['lc_dept_fax_number_field'] ) ? sanitize_text_field($_POST['lc_dept_fax_number_field'] ) : '' );
+
+ /* Get the meta key. */
+ $meta_key = 'lc_dept_fax_number_field';
+
+  /* Get the meta value of the custom field key. */
+ $meta_value = get_post_meta ($post_id, $meta_key, true );
+
+ update_post_meta( $post_id, $meta_key, $new_meta_value, $meta_value );
+
+ /* Room Number Field */
+ /* Get the posted data and sanitize it for use as a date value. */
+ $new_meta_value = ( isset( $_POST['lc_dept_room_number_field'] ) ? sanitize_text_field($_POST['lc_dept_room_number_field'] ) : '' );
+
+ /* Get the meta key. */
+ $meta_key = 'lc_dept_room_number_field';
+
+  /* Get the meta value of the custom field key. */
+ $meta_value = get_post_meta ($post_id, $meta_key, true );
+
+ update_post_meta( $post_id, $meta_key, $new_meta_value, $meta_value );
+
+ /* Office Hours Field */
+ /* Get the posted data and sanitize it for use as a date value. */
+ $new_meta_value = ( isset( $_POST['lc_dept_office_hours_field'] ) ? sanitize_text_field($_POST['lc_dept_office_hours_field'] ) : '' );
+
+ /* Get the meta key. */
+ $meta_key = 'lc_dept_office_hours_field';
+
+  /* Get the meta value of the custom field key. */
+ $meta_value = get_post_meta ($post_id, $meta_key, true );
+
+ update_post_meta( $post_id, $meta_key, $new_meta_value, $meta_value );
+}
+
+
+function update_meta_values( $post_id, $meta_key, $new_meta_value, $meta_value ) {
+
+  /* If a new meta value was added and there was no previous value, add it. */
  if ( $new_meta_value && '' == $meta_value )
    add_post_meta( $post_id, $meta_key, $new_meta_value, true );
- 
+
  /* If the new meta value was added and there was no previous value, add it. */
  elseif ( $new_meta_value && $new_meta_value != $meta_value )
   update_post_meta( $post_id, $meta_key, $new_meta_value );
- 
+
  /* If there is no new meta value but an old value exists, delete it. */
  elseif ( '' == $new_meta_value && $meta_value )
   delete_post_meta( $post_id, $meta_key, $meta_value );
